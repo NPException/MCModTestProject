1cd596144fd953e61285ae46d45384c215725e8e
NPException <npexception42@gmail.com>
2019-03-02 21:54:23 +0100

    implement toString, equals, and hashcode for Cons

--- src/main/java/de/npcomplete/nplisp/data/Cons.java
+++ src/main/java/de/npcomplete/nplisp/data/Cons.java
@@ -1,5 +1,10 @@
 package de.npcomplete.nplisp.data;
 
+import java.util.Iterator;
+import java.util.Objects;
+
+import de.npcomplete.nplisp.util.LispPrinter;
+
 public class Cons implements Sequence {
 
 	private final Object first;
@@ -25,5 +30,44 @@ public class Cons implements Sequence {
 	@Override
 	public boolean empty() {
 		return false;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Sequence)) {
+			return false;
+		}
+		Sequence other = (Sequence) o;
+		if (empty() != other.empty()) {
+			return false;
+		}
+		if (empty() && other.empty()) {
+			return true;
+		}
+		Iterator a = iterator();
+		Iterator b = other.iterator();
+		while (a.hasNext() && b.hasNext()) {
+			if (!Objects.equals(a.next(), b.next())) {
+				return false;
+			}
+		}
+		return !(a.hasNext() || b.hasNext());
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 1;
+		for (Object element : this) {
+			result = 31 * result + Objects.hashCode(element);
+		}
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return LispPrinter.prStr(this);
 	}
 }
